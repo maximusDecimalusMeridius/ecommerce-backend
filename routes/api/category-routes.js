a716d518263b87520acc361669553c97e09e6880
maximusDecimalusMeridius <106376888+maximusDecimalusMeridius@users.noreply.github.com>
2023-02-07 05:39:33 -0800

    Added put and post routes for category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -31,11 +31,34 @@ router.get('/:id', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+  Category.create({
+    category_name: "Test name 2"
+  }).then( data => {
+    res.status(201).json(data);
+  }).catch( error => {
+    res.json({
+      msg: "Error creating new category",
+      error: error
+    })
+  })
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  Category.update({
+    category_name: req.body.category_name
+  },
+  {
+    where: {
+      id: req.params.id
+    }
+  }).then( data => {
+    res.status(200).json(data)
+  }).catch ( error => {
+    res.status(500).json({
+      msg: "There was an error",
+      error: error
+    })
+  })
 });
 
 router.delete('/:id', (req, res) => {
